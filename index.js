435fe04f036907425a7bd57bec6a39901328f998
Gagan S <gagan.s.0175@gmail.com>
2020-06-17 16:28:50 -0700

    update keys and created environment variabled for heroku

--- index.js
+++ index.js
@@ -22,4 +22,6 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
