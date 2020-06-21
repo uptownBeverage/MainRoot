715ebd0e6e2eed49bb0866591cb3a2600e575f21
bhavsingh2017 <bhavkirs@usc.edu>
2020-06-20 22:02:40 -0400

    finished up the model for store, need to figure out how to fill it

--- models/Store.js
+++ models/Store.js
@@ -5,12 +5,15 @@ const {
 
 
 var delivery = {distributor:"Decresente", day:"Monday"};
+var location = {street_address:"123 Connor Ct", zip_code: "12309", city: "albany", state: "New York"}
 
 
 const storeSchema = new Schema({
-  employeeList: Array,
-  deliveryList: Array,
-  pin: String,  
+  employeeList: Array,                  //the employee list
+  deliveryList: Array,                  //the deliveries that the store has every week
+  pin: String,                          //the pin for the store to login
+  address: location,                    //to have exact location of each store
+  contact_info: String,                 //the contact info for the store
 });
 
 mongoose.model('store', storeSchema);
